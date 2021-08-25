1616b0a47d5fad9ddedfde20c44d6e5fbb2fbba9
gongkangjia <gongkangjia@gmail.com>
2021-08-25 15:15:37 +0800

    v0.1

---
+++ dist.sh
@@ -0,0 +1,4 @@
+
+
+
+nuitka --mingw64 --standalone --windows-disable-console --show-progress --show-memory --plugin-enable=tk-inter --plugin-enable=numpy --output-dir=dist --windows-icon-from-ico ./favicon.ico main.py
